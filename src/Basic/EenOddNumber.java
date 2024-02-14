48f4fb2b1482c4d9c2a2ade979bcc1d6b529c6b2
Karan Ray <daliray578@gmail.com>
2024-02-14 18:49:34 +0530

    Even Odd Program

---
+++ src/Basic/EenOddNumber.java
@@ -0,0 +1,9 @@
+package Basic;
+
+public class EenOddNumber {
+    public static void main(String[] args) {
+        int num = 3;
+        if (num % 2 == 0) System.out.println("Even Number");
+        else System.out.println("Odd Number");
+    }
+}
